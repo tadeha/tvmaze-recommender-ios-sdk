fe2db86da82deb5bf82d43770b6aae16ad7fccae
alireza12t <mr.toghyiani@icloud.com>
2021-11-12 21:54:39 +0330

    update Package name

---
+++ Sources/DictionaryExtension.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alireza on 11/12/21.
+//
+
+import Foundation
+
+extension Dictionary {
+    func percentEncoded() -> Data? {
+        return map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+        }
+        .joined(separator: "&")
+        .data(using: .utf8)
+    }
+}
---
+++ Sources/Models.swift
@@ -0,0 +1,20 @@
+//
+//  Models.swift
+//  
+//
+//  Created by Alireza on 11/12/21.
+//
+
+import Foundation
+
+struct SimilarShows: Codable {
+    let similarShow: [String: String]
+    
+    enum CodingKeys: String, CodingKey {
+        case similarShow = "similar_show"
+    }
+}
+
+public struct TVShow: Codable {
+    public let id, name: String
+}
