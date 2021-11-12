f430256543f12b1b58434f4cf0eaba8da1311c6d
alireza12t <mr.toghyiani@icloud.com>
2021-11-12 21:40:14 +0330

    add example

---
+++ Example/Test/ShowTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ShowTableViewCell.swift
+//  Test
+//
+//  Created by Alireza on 11/12/21.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
